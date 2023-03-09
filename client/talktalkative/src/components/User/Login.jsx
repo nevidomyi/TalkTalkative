bac210cc5deaf0a1180ae3095acb95dc10466a64
rrzaevich <47025875+nevidomyi@users.noreply.github.com>
2023-03-09 09:36:00 +0200

    upd
    upd

--- client/talktalkative/src/components/User/Login.jsx
+++ client/talktalkative/src/components/User/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
 
   return (
     <>
-      <div className="login-component">
+      {/* <div className="login-component">
         <div className="header">
           <h1>
             TalkTalkative <img src="public\image\dialog-logo.svg"></img>
@@ -41,13 +41,13 @@ function Login() {
           <input type="button" value="Sign-up" />
           <span>© 2023</span>
         </div>
-      </div>
+      </div> */}
       <div
         id="container"
-        className="flex min-h-full items-center justify-center py-20 px-4 sm:px-6 lg:px-8 relative"
+        className="flex min-h-full items-center justify-center py-20 px-4 sm:px-6 lg:px-8"
       >
         <div id="signup-component" className="w-full max-w-lg space-y-2">
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center mb-10">
             <span className="flex flex-row items-center text-4xl text-awesome-blue h-12">
               <h1>Talk</h1>
               <p className="font-bold ">Talkative</p>
@@ -67,27 +67,32 @@ function Login() {
                 type="text"
                 name="Email"
                 placeholder="Email"
-                className="w-64  h-11 px-8 mt-6 text-l   text-awesome-blue border rounded-3xl border-black transition ease-in-out delay-50 hover:bg-gray-200"
+                className="w-64  h-11 px-6 mt-8 text-l   text-awesome-blue border rounded-3xl border-black transition ease-in-out delay-50 hover:bg-gray-200"
               />
               <input
                 type="password"
                 name="password"
                 placeholder="Password"
-                className="w-64  h-11 px-6 mt-6 text-l  text-awesome-blue border rounded-3xl border-black transition ease-in-out delay-50 hover:bg-gray-200"
+                className="w-64  h-11 px-6 mt-8 text-l  text-awesome-blue border rounded-3xl border-black transition ease-in-out delay-50 hover:bg-gray-200"
               />
+              <div className="flex items-center mt-14">
+                <input
+                  type="checkbox"
+                  name="rmbm"
+                  id="rmbm"
+                  className="mr-3 border-black rounded-md"
+                />
+                <label htmlFor="rmbm">Remember me</label>
+              </div>
               <input
                 type="button"
-                value="Sign-up"
-                className="w-32 h-12 mt-5 bg-awesome-red rounded-2xl text-white text-xl transition ease-in-out delay-50 hover:shadow-inner hover:bg-hower-aw-red cursor-pointer"
+                value="Sign-in"
+                className="w-32 h-12 mt-10 mb-6 bg-awesome-blue rounded-2xl text-white text-xl transition ease-in-out delay-50 hover:shadow-inner hover:bg-hower-aw-blue cursor-pointer"
               />
             </form>
           </div>
-
-          <div className="flex justify-center mt-6">
-            <span className="text-center">© 2023</span>
-          </div>
-          <div id="signin" className="flex flex-row absolute top-3 right-3">
-            <span className="break-words w-44 mr-2">
+          <div id="signin" className="flex flex-col items-center">
+            <span className="break-words mr-2">
               You already have profile? Not a problem
             </span>
             <a
@@ -96,6 +101,9 @@ function Login() {
             >
               <p className="text-inherit">Sign-in</p>
             </a>
+          </div>
+          <div className="flex justify-center mt-6">
+            <span className="text-center">© 2023</span>
           </div>
         </div>
       </div>
