cad1393f4a915d3180f8c4687215960a4e084680
rrzaevich <47025875+nevidomyi@users.noreply.github.com>
2023-05-02 16:04:01 +0300

    Update

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -6,5 +6,15 @@ const chatController = {
     const messages = await chatService.getMessages();
     res.json(messages);
   }),
+
+  getChats: asyncHandler(async (req, res) => {
+    const chats = await chatService.getChats();
+    res.json(chats);
+  }),
+
+  getCategories: asyncHandler(async (req, res) => {
+    const categories = await chatService.getCategories();
+    res.json(categories);
+  }),
 };
 module.exports = chatController;
